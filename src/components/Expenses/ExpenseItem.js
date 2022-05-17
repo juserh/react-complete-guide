c8cd980c8b6d3559fb924a9fa57c79420ca06717
juserh <rosapark07@gmail.com>
2022-05-17 16:33:59 +0900

    데이터 배열의 동적 변환을 위해 useState 사용

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,6 @@
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-import { useState } from "react";
 
 const ExpenseItem = (props) => {
   console.log("ExpenseItem evaluated by React");
