dcd708f1d5cc3c1f877d092a312458804c638ee4
juserh <rosapark07@gmail.com>
2022-05-21 13:00:11 +0900

    conditional content-form 수정(강의 정답)

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,14 +2,6 @@ import React, { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = (props) => {
-  const [visible, setVisible] = useState(false);
-
-  const clickHandler = () => {
-    setVisible((prevState) => {
-      return !prevState;
-    });
-  };
-
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -56,14 +48,7 @@ const ExpenseForm = (props) => {
     setEnteredTitle("");
     setEnteredAmount("");
     setEnteredDate("");
-    setVisible((prevState) => {
-      return !prevState;
-    });
   };
-
-  if (visible === false) {
-    return <button onClick={clickHandler}>Add Expense</button>;
-  }
 
   return (
     <form onSubmit={submitHandler}>
@@ -98,7 +83,9 @@ const ExpenseForm = (props) => {
         </div>
       </div>
       <div className="new-expense__actions">
-        <button onClick={clickHandler}>Cancel</button>
+        <button type="button" onClick={props.onCancel}>
+          Cancel
+        </button>
         <button type="submit">Add Expense</button>
       </div>
     </form>
